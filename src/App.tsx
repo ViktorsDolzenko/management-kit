a3595397de943c6d71ddd9606f6d6de0be2676af
AwSicker <vitja1234@gmail.com>
2021-01-01 13:04:14 +0200

    need review

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Header} from "./components/layout/Header/Header";
+import "./scss/styles.scss"
+import {SideBar} from "./components/layout/SideBar/SideBar";
 
 export const App = () => {
   return (
       <div>
-        Hello
+  <Header/>
+  <SideBar/>
       </div>
   );
 }
---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Logo from "../../../svg/logo.svg"
+
+export const Header = () => {
+    return (
+       <header className="page-header">
+           <nav className="navbar">
+               <div className="page-header__main">
+                   <div className="page-header__left-wrapper">
+           <a href="/" className="page-header__logo">
+               <img className="page-header__img" src={Logo} alt="logo" />
+               <span className="page-header__title">WebSite</span>
+           </a>
+                   <button className="navbar__button showmore">
+                       <i className="fas fa-ellipsis-h"/>
+                   </button>
+                   </div>
+                   <div>
+                   <button className="navbar__button share">Share</button>
+                   <button className="navbar__button chat">Chat</button>
+                   </div>
+               </div>
+             <div>
+                 <ul className="navbar__list nav-list">
+                     <li className="nav-list--item"><a>Tasks</a></li>
+                     <li className="nav-list--item"><a>Kanban</a></li>
+                     <li className="nav-list--item"><a>Activity</a></li>
+                     <li className="nav-list--item"><a>Calendar</a></li>
+                     <li className="nav-list--item"><a>Files</a></li>
+                 </ul>
+             </div>
+           </nav>
+       </header>
+    );
+};
